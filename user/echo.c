f67cd63b56c6c058b7c8b25ca6891e3231a82ce2
kali-rayyanrk7 <kali-rayyanrk7@kali-rayyanrk7.kali-rayyanrk7>
2025-09-23 18:19:49 +0500

    Add sleep user program (Problem 1)

--- user/echo.c
+++ user/echo.c
@@ -1,19 +1,15 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
+  for(int i = 1; i < argc; i++){
+    printf("%s", argv[i]);
     if(i + 1 < argc){
-      write(1, " ", 1);
-    } else {
-      write(1, "\n", 1);
+      printf(" ");
     }
   }
+  printf("\n");
   exit(0);
 }
