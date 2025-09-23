ea2fce3d31d29e926ee44f683931bebf0c51b44d
kali-rayyanrk7 <kali-rayyanrk7@kali-rayyanrk7.kali-rayyanrk7>
2025-09-23 20:50:18 +0500

    ProblemSet 1 completed

--- Makefile
+++ Makefile
@@ -190,6 +190,7 @@ UPROGS=\
 	$U/_rm\
 	$U/_sh\
 	$U/_stressfs\
+	$U/_sleep\
 	$U/_sixfive\
 	$U/_usertests\
 	$U/_grind\
---
+++ user/sleep.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(argc < 2){
+        fprintf(2, "Usage: sleep <ticks>\n");
+        exit(1);
+    }
+
+    int ticks = atoi(argv[1]);
+    if(ticks < 0){
+        fprintf(2, "sleep: invalid number of ticks\n");
+        exit(1);
+    }
+
+    // pause() sleeps for given ticks
+    pause(ticks);
+    exit(0);
+}
